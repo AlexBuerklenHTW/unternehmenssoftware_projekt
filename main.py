1cd2237b302e7e47c851ebe30b41f418f3243120
unknown <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-11-04 19:03:33 +0100

    update

--- main.py
+++ main.py
@@ -35,67 +35,20 @@ openai.api_key = open(file_path, "r").read()
 
 ## Code below sets a context with the role set to "system".
 
-# chat_log = []
-
-# Initialize the conversation with ChatGPT acting as a seller
-# chat_log.append({"role": "system", "content": "You are a seller of a phone. The price of the phone is 800€."})
-#
-# while True:
-#     user_message = input("User: ")
-#     if user_message.lower() == "quit":
-#         break
-#
-#     # User interaction
-#     chat_log.append({"role": "user", "content": user_message})
-#
-#     # Generate a response from ChatGPT
-#     response = openai.ChatCompletion.create(
-#         model="gpt-3.5-turbo",
-#         messages=chat_log
-#     )
-#     assistant_response = response['choices'][0]['message']['content']
-#     print("ChatGPT:", assistant_response.strip())
-#
-#     # Add ChatGPT's response to the conversation log
-#     chat_log.append({"role": "assistant", "content": assistant_response})
-
-def assistant_1(chat_log):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=chat_log
-    )
-    return response['choices'][0]['message']['content']
-
-
-def assistant_2(chat_log):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=chat_log
-    )
-    return response['choices'][1]['message']['content']
-
-
-# Initialize the conversation with two AI "assistants"
-chat_log = [
-    {"role": "assistant_1", "content": "Hello, I am Assistant 1."},
-    {"role": "assistant_2", "content": "Hi, I am Assistant 2."}
-]
+chat_agent_seller = [{"role": "system", "content": "You are a seller of a phone. The price of the phone is 800€."}]
+chat_agent_buyer = [{"role": "system", "content": "You are a buyer of a phone. You want to buy it for 600€"}]
+chat_response_seller = [{"role": "assistant", "content": ""}]
+chat_response_buyer = []
 
 while True:
-    user_message = input("User: ")
-    if user_message.lower() == "quit":
-        break
 
-    # User interaction (optional)
-    chat_log.append({"role": "user", "content": user_message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=chat_response_seller
+    )
+    assistant_response = response['choices'][0]['message']['content']
+    print("ChatGPT:", assistant_response.strip())
 
-    # Get responses from both assistants
-    assistant_1_response = assistant_1(chat_log)
-    assistant_2_response = assistant_2(chat_log)
+    # Add ChatGPT's response to the conversation log
+    chat_log.append({"role": "assistant", "content": assistant_response})
 
-    # Print and update the conversation log
-    print("Assistant 1:", assistant_1_response.strip())
-    print("Assistant 2:", assistant_2_response.strip())
-
-    chat_log.append({"role": "assistant_1", "content": assistant_1_response})
-    chat_log.append({"role": "assistant_2", "content": assistant_2_response})

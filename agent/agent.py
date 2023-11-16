b0d9aa0846a7023e78f6b15943415863b918125c
unknown <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-11-16 15:14:43 +0100

    added new method with temperature tag, generated examples and update documentary

--- agent/agent.py
+++ agent/agent.py
@@ -25,6 +25,28 @@ def agent_java_and_python_names():
     print(response['choices'][0]['message']['content'])
 
 
+def agent_java_and_python_names_temperature():
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        temperature=0,
+        messages=[
+            {"role": "assistant",
+             "content": "You take the role of a Java expert (Paul). You learned this programming language since you started learning programming."
+                        "You worked in a lot of companies and on a lot of projects. You think, Java is the best language"
+                        "and that you can solve any problem with Java."
+                        "You work for Company X. Company X wants to decide between java and python for backend. You are going to "
+                        "defend Java in the discussion."},
+            {"role": "assistant",
+             "content": "You take the role of a Python expert (Sarah). You learned this programming language since you started learning programming."
+                        "You worked in a lot of companies and on a lot of projects. You think, Python is the best language"
+                        "and that you can solve any problem with Python."
+                        "You work for Company X. Company X wants to decide between java and python for backend. You are going to "
+                        "defend Python in the discussion."}
+        ]
+    )
+    print(response['choices'][0]['message']['content'])
+
+
 def agent_java_and_python_no_names():
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import openai
 
-from agent.agent import agent_java_and_python_no_names, agent_java_and_python_names, \
-    agent_java_and_python_names_continue
-from buyer_and_seller.buyer_and_seller import bas_sbp, bas_evaluator, bas_assistant
+from agent.agent import agent_java_and_python_names, agent_java_and_python_names_temperature
 
 file_path = "API_KEY"
 openai.api_key = open(file_path, "r").read()
@@ -11,5 +9,4 @@ openai.api_key = open(file_path, "r").read()
 # optionale tags reserchieren (z.B.: seed)
 # zwei extremen gegeneinander laufen lassen
 
-agent_java_and_python_names_continue()
-
+agent_java_and_python_names_temperature()

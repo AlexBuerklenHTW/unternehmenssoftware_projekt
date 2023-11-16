3429f70694a67b1bc7755fbb9d6e0abda2d8584f
unknown <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-11-16 12:58:10 +0100

    finalized agent.py

--- agent/agent.py
+++ agent/agent.py
@@ -1,40 +1,46 @@
 import openai
 
-
 file_path = "API_KEY"
 openai.api_key = open(file_path, "r").read()
 
 
-def agent_java_and_python():
+def agent_java_and_python_names():
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system",
-             "content": "You are a Java expert. You learned this programming language since you started learning programming."
+            {"role": "assistant",
+             "content": "You take the role of a Java expert (Paul). You learned this programming language since you started learning programming."
                         "You worked in a lot of companies and on a lot of projects. You think, Java is the best language"
-                        "and that you can solve any problem with Java."},
-            {"role": "system",
-             "content": "You are a Python expert. You learned this programming language since you started learning programming."
+                        "and that you can solve any problem with Java."
+                        "You work for Company X. Company X wants to decide between java and python for backend. You are going to "
+                        "defend Java in the discussion."},
+            {"role": "assistant",
+             "content": "You take the role of a Python expert (Sarah). You learned this programming language since you started learning programming."
                         "You worked in a lot of companies and on a lot of projects. You think, Python is the best language"
-                        "and that you can solve any problem with Python."}
+                        "and that you can solve any problem with Python."
+                        "You work for Company X. Company X wants to decide between java and python for backend. You are going to "
+                        "defend Python in the discussion."}
         ]
     )
-    return response['choices'][0]['message']['content']
+    print(response['choices'][0]['message']['content'])
 
 
-def start_conversation():
-
-    starter = openai.ChatCompletion.create(
+def agent_java_and_python_no_names():
+    response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system",
-             "content": "We need to build a E-Commerce platform, where we sell computer hardware. What programming language should we use for backend? Java or python?"},
             {"role": "assistant",
-             "content": agent_java()},
+             "content": "You take the role of a Java expert. You learned this programming language since you started learning programming."
+                        "You worked in a lot of companies and on a lot of projects. You think, Java is the best language"
+                        "and that you can solve any problem with Java."
+                        "You work for Company X. Company X wants to decide between java and python for backend. You are going to "
+                        "defend Java in the discussion."},
             {"role": "assistant",
-             "content": agent_python()}
+             "content": "You take the role of a Python expert. You learned this programming language since you started learning programming."
+                        "You worked in a lot of companies and on a lot of projects. You think, Python is the best language"
+                        "and that you can solve any problem with Python."
+                        "You work for Company X. Company X wants to decide between java and python for backend. You are going to "
+                        "defend Python in the discussion."}
         ]
     )
-
-    print(starter['choices'][0]['message']['content'])
-
+    print(response['choices'][0]['message']['content'])
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import openai
 
-from agent.agent import start_conversation
-from buyer_and_seller.buyer_and_seller import bas_sbp, bas_evaluator
+from agent.agent import agent_java_and_python_no_names, agent_java_and_python_names, \
+    agent_java_and_python_names_continue
+from buyer_and_seller.buyer_and_seller import bas_sbp, bas_evaluator, bas_assistant
 
 file_path = "API_KEY"
 openai.api_key = open(file_path, "r").read()
@@ -10,4 +11,5 @@ openai.api_key = open(file_path, "r").read()
 # optionale tags reserchieren (z.B.: seed)
 # zwei extremen gegeneinander laufen lassen
 
-bas_evaluator()
+agent_java_and_python_names_continue()
+

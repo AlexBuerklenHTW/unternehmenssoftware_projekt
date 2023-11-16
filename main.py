f44568ef3f5673705d2cc243eb62ae25bcc984cc
unknown <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-11-16 16:15:00 +0100

    added new agent method with top_g parameter and updated documentation

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import openai
 
-from agent.agent import agent_java_and_python_names, agent_java_and_python_names_temperature
-
 file_path = "API_KEY"
 openai.api_key = open(file_path, "r").read()
 
 # step-back-prompting
-# optionale tags reserchieren (z.B.: seed)
-# zwei extremen gegeneinander laufen lassen
-
-agent_java_and_python_names_temperature()

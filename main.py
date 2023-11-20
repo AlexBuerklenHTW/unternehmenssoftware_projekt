2e0f10ccd53fcc017669ed10c811ffbc09246bab
Alexander Bürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-11-20 09:29:02 +0100

    added comments and ideas

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ file_path = "API_KEY"
 openai.api_key = open(file_path, "r").read()
 
 # step-back-prompting
+# testen, ob name wirklich unterschied macht
+# streamlit website anschauen
+# chromadb anschauen
+# max_tokens als limit setzen

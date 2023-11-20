cc90074d3274dbe0105c8a89ae9aaa8a05df8b68
unknown <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-11-20 14:07:46 +0100

    added streamlit

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import openai
-
-file_path = "API_KEY"
-openai.api_key = open(file_path, "r").read()
+import streamlit as st
 
 # step-back-prompting
 # testen, ob name wirklich unterschied macht
 # streamlit website anschauen
 # chromadb anschauen
 # max_tokens als limit setzen
+
+file_path = "API_KEY"
+openai.api_key = open(file_path, "r").read()
+
+st.title("The app title")

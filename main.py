ac4020091d03e95ddc8ce463b4a1471c0b5331bb
unknown <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-11-20 14:14:54 +0100

    deleted import openai

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import streamlit as st
-import openai
-
 # step-back-prompting
 # testen, ob name wirklich unterschied macht
 # streamlit website anschauen
 # chromadb anschauen
 # max_tokens als limit setzen
 
-file_path = "API_KEY"
-openai.api_key = open(file_path, "r").read()
 
-st.title("The app title")

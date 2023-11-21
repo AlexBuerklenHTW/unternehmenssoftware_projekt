c3254bae05562f34231ed443ae273ed35a76463d
unknown <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-11-21 19:28:06 +0100

    added loop for adding agents (unfinished)

--- main.py
+++ main.py
@@ -6,38 +6,59 @@
 
 import streamlit as st
 
+def main():
+    st.write(st.session_state)
+    create_topic()
+    create_agent()
+    #add_agent()
+
+
 topic = []
 agents = []
+counter = 0
 
-if "disabled" not in st.session_state:
-    st.session_state["disabled"] = False
+if "disabled_topic" not in st.session_state:
+    st.session_state["disabled_topic"] = False
+
+if "disabled_agent" not in st.session_state:
+    st.session_state["disabled_agent"] = False
+
 
 
 def disable_topic():
-    st.session_state["disabled"] = True
-
+    st.session_state["disabled_topic"] = True
 
 def disable_agent():
-    st.session_state["disabled"] = True
+    st.session_state["disabled_agent"] = True
 
+def enable_agent():
+    st.session_state["disabled_agent"] = False
 
-def main():
-    create_topic()
-    create_agent()
+# def add_agent():
+#     button = st.button("click here to add agent")
+#     if button:
+#         create_agent()
 
 
 def create_topic():
-    topic_entry = st.text_input("Enter topic:", disabled=st.session_state.disabled, on_change=disable_topic())
+    topic_entry = st.text_input("Enter topic:", disabled=st.session_state["disabled_topic"], on_change=disable_topic)
     if topic_entry:
         topic.append(topic_entry)
-        st.write(f"Topic: {topic_entry} (If you want a new topic, than refresh the page)")
+        st.write(f"Topic: {topic_entry}")
 
 
 def create_agent():
-    agent_entry = st.text_input("Enter name of agent:", disabled=st.session_state.disabled, on_change=disable_agent(), key="create_agent")
+    global counter
+    agent_entry = st.text_input("Enter name of agent:", disabled=st.session_state["disabled_agent"], on_change=disable_agent, key=counter)
+    counter += 1
     if agent_entry:
-        topic.append(agent_entry)
-        st.write(f"Topic: {agent_entry} (If you want a new topic, than refresh the page)")
+        agents.append(agent_entry)
+        st.write(f"Agent name: {agent_entry}")
+        button = st.button("click here to add agent")
+        if button:
+            enable_agent()
+            create_agent()
+
 
 
 if __name__ == "__main__":

1f24a346785dc207059592752266ee12a91b66b0
unknown <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-11-22 14:43:44 +0100

    finished user input and added header

--- main.py
+++ main.py
@@ -6,38 +6,64 @@
 
 import streamlit as st
 
+
 def main():
-    st.write(st.session_state)
+    st.header("Lets start a discussion! Enter a topic you want to discuss and create up to 3 agents")
     create_topic()
     create_agent()
-    #add_agent()
 
 
 topic = []
 agents = []
-counter = 0
+
+if 'stage' not in st.session_state:
+    st.session_state.stage = 0
 
 if "disabled_topic" not in st.session_state:
     st.session_state["disabled_topic"] = False
 
-if "disabled_agent" not in st.session_state:
-    st.session_state["disabled_agent"] = False
+if "disabled_agent_1" not in st.session_state:
+    st.session_state["disabled_agent_1"] = False
 
+if "disabled_agent_2" not in st.session_state:
+    st.session_state["disabled_agent_2"] = False
+
+if "disabled_agent_3" not in st.session_state:
+    st.session_state["disabled_agent_3"] = False
+
+if "disabled_add_button_1" not in st.session_state:
+    st.session_state["disabled_add_button_1"] = False
+
+if "disabled_add_button_2" not in st.session_state:
+    st.session_state["disabled_add_button_2"] = False
 
 
 def disable_topic():
     st.session_state["disabled_topic"] = True
 
-def disable_agent():
-    st.session_state["disabled_agent"] = True
 
-def enable_agent():
-    st.session_state["disabled_agent"] = False
+def disable_agent_1():
+    st.session_state["disabled_agent_1"] = True
 
-# def add_agent():
-#     button = st.button("click here to add agent")
-#     if button:
-#         create_agent()
+
+def disable_agent_2():
+    st.session_state["disabled_agent_2"] = True
+
+
+def disable_agent_3():
+    st.session_state["disabled_agent_3"] = True
+
+
+def disable_add_button_1():
+    st.session_state["disabled_add_button_1"] = True
+
+
+def disable_add_button_2():
+    st.session_state["disabled_add_button_2"] = True
+
+
+def set_state():
+    st.session_state.stage += 1
 
 
 def create_topic():
@@ -48,16 +74,33 @@ def create_topic():
 
 
 def create_agent():
-    global counter
-    agent_entry = st.text_input("Enter name of agent:", disabled=st.session_state["disabled_agent"], on_change=disable_agent, key=counter)
-    counter += 1
-    if agent_entry:
-        agents.append(agent_entry)
-        st.write(f"Agent name: {agent_entry}")
-        button = st.button("click here to add agent")
-        if button:
-            enable_agent()
-            create_agent()
+    if st.session_state.stage == 0:
+        st.button('Add agent', on_click=set_state)
+
+    if st.session_state.stage >= 1:
+        agent_name = st.text_input('Name', on_change=set_state and disable_agent_1, key="name_agent_1",
+                                   disabled=st.session_state["disabled_agent_1"])
+        st.write(f"Name of Agent 1: {agent_name}")
+        agents.append(agent_name)
+        if agent_name:
+            st.button('Add agent', disabled=st.session_state["disabled_add_button_1"], on_click=set_state, key="")
+            disable_add_button_1()
+
+            if st.session_state.stage >= 2:
+                agent_name = st.text_input('Name', on_change=set_state and disable_agent_2, key="name_agent_2",
+                                           disabled=st.session_state["disabled_agent_2"])
+                st.write(f"Name of Agent 2: {agent_name}")
+                agents.append(agent_name)
+                if agent_name:
+                    st.button('Add agent', disabled=st.session_state["disabled_add_button_2"], on_click=set_state,
+                              key=4)
+                    disable_add_button_2()
+
+                    if st.session_state.stage >= 3:
+                        agent_name = st.text_input('Name', on_change=set_state and disable_agent_3, key="name_agent_3",
+                                                   disabled=st.session_state["disabled_agent_3"])
+                        st.write(f"Name of Agent 3: {agent_name}")
+                        agents.append(agent_name)
 
 
 

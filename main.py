4913609ae4dd4eb43406d48821e448aae60d9d83
unknown <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-11-23 18:43:15 +0100

    UI finished

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # step-back-prompting
-# testen, ob name wirklich unterschied macht
-# streamlit website anschauen
 # chromadb anschauen
 # max_tokens als limit setzen
 
@@ -10,25 +8,36 @@ import streamlit as st
 file_path = "API_KEY"
 openai.api_key = open(file_path, "r").read()
 
+agents_name_description = {}
+topic = []
 
 
 def main():
-    # st.chat_input("Say something")
     with st.sidebar:
-        st.header("Lets start a discussion! Enter a topic you want to discuss and create up to 3 agents")
+        st.header(
+            "Lets start a discussion! Enter a topic you want to discuss and create 3 agents. (Chat input is going to be available after creating agents)")
         create_topic()
-        create_agent_name()
+        # st.write(topic)
+        create_agent_name_and_description()
+        # st.write(agents_name_description)
+    if st.session_state.stage_agent >= 4:
+        with st.chat_message("assistant"):
+            disable_chat_input()
+            agents(agents_name_description)
+    #st.chat_input("Say something", disabled=st.session_state["disabled_chat_input"])
 
-
-
-topic = []
-agents = {}
 
 if 'stage_agent' not in st.session_state:
     st.session_state.stage_agent = 0
 
 if "disabled_topic" not in st.session_state:
     st.session_state["disabled_topic"] = False
+
+if "disabled_chat_input" not in st.session_state:
+    st.session_state["disabled_chat_input"] = True
+
+if "disabled_finish_button" not in st.session_state:
+    st.session_state["disabled_finish_button"] = False
 
 if "disabled_agent_1" not in st.session_state:
     st.session_state["disabled_agent_1"] = False
@@ -59,14 +68,25 @@ def disable_topic():
     st.session_state["disabled_topic"] = True
 
 
+def disable_chat_input():
+    st.session_state["disabled_chat_input"] = False
+
+
+def disable_finish_button():
+    st.session_state["disabled_finish_button"] = True
+
+
 def disable_agent_1():
     st.session_state["disabled_agent_1"] = True
+
 
 def disable_agent_description_1():
     st.session_state["disabled_agent_description_1"] = True
 
+
 def disable_agent_description_2():
     st.session_state["disabled_agent_description_2"] = True
+
 
 def disable_agent_description_3():
     st.session_state["disabled_agent_description_3"] = True
@@ -90,30 +110,36 @@ def disable_add_button_2():
 
 def set_state():
     st.session_state.stage_agent += 1
-    #st.session_state.stage_description += 1
 
 
-def agent(name):
+def agents(agents_dict):
+    names = list(agents_dict.keys())
+    description = list(agents_dict.values())
+    #temperature funktioniert irgendwie nicht. Assistant funktioniert irgendwie nicht, aber System läuft besser.
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
+        #temperature=0,
         messages=[
             {"role": "system",
-             "content": f"name: {name}."}
+             "content": f"name: {names[0]}.  description: {description[0]}. topic: {topic}"},
+            {"role": "system",
+             "content": f"name: {names[1]}. description: {description[1]}. topic: {topic}"},
+            {"role": "system",
+             "content": f"name: {names[2]}. description: {description[2]}. topic: {topic}"}
         ]
     )
-    print(response['choices'][0]['message']['content'])
+    st.write(response['choices'][0]['message']['content'])
 
 
 def create_topic():
+    global topic
     topic_entry = st.text_input("Enter topic:", disabled=st.session_state["disabled_topic"], on_change=disable_topic)
     if topic_entry:
         topic.append(topic_entry)
         st.write(f"Topic: {topic_entry}")
 
 
-
-
-def create_agent_name():
+def create_agent_name_and_description():
     if st.session_state.stage_agent == 0:
         st.button('Add agent', on_click=set_state)
 
@@ -122,18 +148,16 @@ def create_agent_name():
                                      on_change=disable_agent_1,
                                      disabled=st.session_state["disabled_agent_1"])
         st.write(f"Name of Agent 1: {agent_name_1}")
-        agent_name_1_description = st.text_input("Description",
-                                                 on_change=set_state and disable_agent_description_1,
-                                                 disabled=st.session_state["disabled_agent_description_1"],
-                                                 key="agent_1")
-        st.write(f"Description of Agent {agent_name_1}: {agent_name_1_description}")
-        #agents.update({"agent_1": agent_name_1})
-        #create_agent_description(agents.get("agent_1"))
+        agent_description_1 = st.text_input("Description",
+                                            on_change=set_state and disable_agent_description_1,
+                                            disabled=st.session_state["disabled_agent_description_1"],
+                                            key="agent_1")
+        st.write(f"Description of Agent {agent_name_1}: {agent_description_1}")
+        agents_name_description[f"{agent_name_1}"] = f"{agent_description_1}"
 
-        if agent_name_1 and agent_name_1_description:
+        if agent_name_1 and agent_description_1:
             st.button('Add agent', disabled=st.session_state["disabled_add_button_1"], on_click=set_state, key="")
             disable_add_button_1()
-
 
             if st.session_state.stage_agent >= 2:
                 agent_name_2 = st.text_input('Name',
@@ -141,13 +165,14 @@ def create_agent_name():
                                              disabled=st.session_state["disabled_agent_2"],
                                              key="name_agent_2")
                 st.write(f"Name of Agent 2: {agent_name_2}")
-                agent_name_2_description = st.text_input("Description",
-                                                         on_change=set_state and disable_agent_description_2,
-                                                         disabled=st.session_state["disabled_agent_description_2"],
-                                                         key="agent_2")
-                st.write(f"Description of Agent {agent_name_2}: {agent_name_2_description}")
+                agent_description_2 = st.text_input("Description",
+                                                    on_change=set_state and disable_agent_description_2,
+                                                    disabled=st.session_state["disabled_agent_description_2"],
+                                                    key="agent_2")
+                st.write(f"Description of Agent {agent_name_2}: {agent_description_2}")
+                agents_name_description[f"{agent_name_2}"] = f"{agent_description_2}"
 
-                if agent_name_2 and agent_name_2_description:
+                if agent_name_2 and agent_description_2:
                     st.button('Add agent', disabled=st.session_state["disabled_add_button_2"], on_click=set_state,
                               key=4)
                     disable_add_button_2()
@@ -158,13 +183,17 @@ def create_agent_name():
                                                      disabled=st.session_state["disabled_agent_3"],
                                                      key="name_agent_3")
                         st.write(f"Name of Agent 3: {agent_name_3}")
-                        agent_name_3_description = st.text_input("Description",
-                                                                 on_change=set_state and disable_agent_description_3,
-                                                                 disabled=st.session_state[
-                                                                     "disabled_agent_description_3"],
-                                                                 key="agent_3")
-                        st.write(f"Description of Agent {agent_name_3}: {agent_name_3_description}")
-                        #agents.append(agent_name_3)
+                        agent_description_3 = st.text_input("Description",
+                                                            on_change=set_state and disable_agent_description_3,
+                                                            disabled=st.session_state[
+                                                                "disabled_agent_description_3"],
+                                                            key="agent_3")
+                        st.write(f"Description of Agent {agent_name_3}: {agent_description_3}")
+                        agents_name_description[f"{agent_name_3}"] = f"{agent_description_3}"
+                        if agent_name_3 and agent_description_3:
+                            st.button('Start', disabled=st.session_state["disabled_finish_button"], key="finish_button",
+                                      on_click=set_state)
+                            disable_finish_button()
 
 
 if __name__ == "__main__":

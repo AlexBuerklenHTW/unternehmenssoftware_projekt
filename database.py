d5bce17a66047e20e70373dbca5a4aba96b8001a
Alexander Bürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-12-03 15:40:39 +0100

    cleanup

--- database.py
+++ database.py
@@ -4,18 +4,18 @@ chroma_client = db.Client()
 
 collection = chroma_client.get_or_create_collection(name="main_collection")
 
-collection.add(
-    ids=["id1", "id2", "id3"],
-    documents=["This is a document", "This is another document", "industrie"]
-)
-
-results = collection.query(
-    query_texts=["This is a query document"]
-)
-
-for inner_list in results["documents"]:
-    text = ", ".join(inner_list)
-    print(text)
+# collection.add(
+#     ids=["id1", "id2", "id3"],
+#     documents=["This is a document", "This is another document", "industrie"]
+# )
+#
+# results = collection.query(
+#     query_texts=["This is a query document"]
+# )
+#
+# for inner_list in results["documents"]:
+#     text = ", ".join(inner_list)
+#     print(text)
 
 
 #print(results["documents"])

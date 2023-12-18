6eb6d29f47c83f8384ebe68983be562c986bc1df
Alexander Bürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-12-18 12:45:25 +0100

    working conversation/discussion works

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import logging
-
 import streamlit as st
 import openai
-import database as db
+import chromadb as db
 
 client = openai
+
+chroma_client = db.Client()
+
+collection = chroma_client.get_or_create_collection(name="main_collection")
 
 file_path = "API_KEY"
 client.api_key = open(file_path, "r").read()
 
 agents_name_description = {}
 topic = []
+
 
 def main():
     with st.sidebar:
@@ -19,8 +23,8 @@ def main():
         create_topic()
         create_agent_name_and_description()
     if st.session_state.stage_agent >= 4:
-        agents_db(agents_name_description)
-        chat_db()
+        # agents_db(agents_name_description)
+        chat_db(agents_name_description)
 
 
 if "messages" not in st.session_state:
@@ -115,7 +119,10 @@ def set_state():
     st.session_state.stage_agent += 1
 
 
-def chat_db():
+def chat_db(agents_dict):
+    names = list(agents_dict.keys())
+    description = list(agents_dict.values())
+
     if prompt := st.chat_input("Type something"):
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
@@ -125,53 +132,46 @@ def chat_db():
             message_placeholder = st.empty()
             full_response = ""
 
-            query = db.collection.query(query_texts=[prompt])
+            query = collection.query(query_texts=[prompt])
             for inner_list in query["documents"]:
                 text = ", ".join(inner_list)
             # role system, wo der text ist funktioniert viel besser, als assistant
+            logging.info(query)
             for response in client.ChatCompletion.create(
                     model="gpt-3.5-turbo",
                     temperature=0,
                     messages=[{"role": "system", "content": text},
+                              {"role": "system",
+                               "content": f"You take the role of: {names[0]}. Your description is: {description[0]}."
+                                          f"The topic you going to discuss about: {topic}. You only gonna take position for your description and defend your position as best as you can."
+                                          f"The point is, that the discussion should not be one sided, more back and forth. You going to answer in the language of the user prompt."
+                                          f" Always reply with your name, For example: 'your name': ... "},
+                              {"role": "system",
+                               "content": f"You take the role of: {names[1]}. Your description is: {description[1]}."
+                                          f"The topic you going to discuss about: {topic}. You only gonna take position for your description and defend your position as best as you can."
+                                          f"The point is, that the discussion should not be one sided, more back and forth. You going to answer in the language of the user prompt."
+                                          f"Always reply with your name, For example: 'your name': ... "},
+                              {"role": "system",
+                               "content": f"You take the role of: {names[2]}. Your description is: {description[2]}."
+                                          f"The topic you going to discuss about: {topic}. You only gonna take position for your description and defend your position as best as you can."
+                                          f"The point is, that the discussion should not be one sided, more back and forth. You going to answer in the language of the user prompt."
+                                          f"Always reply with your name, For example: 'your name': ... "},
                               {"role": "user",
                                "content": prompt}],
                     stream=True,
             ):
-                db.collection.add(
-                    documents=[full_response],
-                    ids=["id"]
-                )
+                logging.info(response)
                 full_response += response.choices[0].delta.get("content", "")
+                ids = 0
+                if not collection.get(str(ids)):
+                    collection.add(
+                        documents=[full_response],
+                        ids=[str(ids)]
+                    )
+                    ids += 1
                 message_placeholder.markdown(full_response + "▌")
             message_placeholder.markdown(full_response)
             st.session_state.messages.append({"role": "assistant", "content": full_response})
-
-
-def agents_db(agents_dict):
-
-    names = list(agents_dict.keys())
-    description = list(agents_dict.values())
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system",
-             "content": f"You take the role of: {names[0]}. Your description is: {description[0]}."
-                        f"The topic you going to discuss about: {topic}. You only gonna take position for your description and defend your position as best as you can."
-                        f"The point is, that the discussion should not be one sided, more back and forth."},
-            {"role": "system",
-             "content": f"You take the role of: {names[1]}. Your description is: {description[1]}."
-                        f"The topic you going to discuss about: {topic}. You only gonna take position for your description and defend your position as best as you can."
-                        f"The point is, that the discussion should not be one sided, more back and forth."},
-            {"role": "system",
-             "content": f"You take the role of: {names[2]}. Your description is: {description[2]}."
-                        f"The topic you going to discuss about: {topic}. You only gonna take position for your description and defend your position as best as you can."
-                        f"The point is, that the discussion should not be one sided, more back and forth."}
-        ]
-    )
-    db.collection.add(
-        documents=[response['choices'][0]['message']['content']],
-        ids=["id"]
-    )
 
 
 def create_topic():
